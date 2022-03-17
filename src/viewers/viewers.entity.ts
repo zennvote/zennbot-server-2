5d2e8b404432e8ff6161129b3b09299f96f90af9
XxshiftxX <contact@dev-shift.me>
2022-03-18 00:03:00 +0900

    ViewersRepository 조회 구현

--- src/viewers/viewers.entity.ts
+++ src/viewers/viewers.entity.ts
@@ -1,7 +1,23 @@
+export interface ViewerInitializer {
+  username: string;
+  twitchId?: string;
+  ticket: number;
+  ticketPiece: number;
+  prefix?: string;
+}
+
 export class Viewer {
   username: string;
   twitchId?: string;
   ticket: number;
   ticketPiece: number;
   prefix?: string;
+
+  constructor(initializer?: ViewerInitializer) {
+    this.username = initializer?.username;
+    this.twitchId = initializer?.twitchId;
+    this.ticket = initializer?.ticket;
+    this.ticketPiece = initializer?.ticketPiece;
+    this.prefix = initializer?.prefix;
+  }
 }
--- src/viewers/viewers.repository.ts
+++ src/viewers/viewers.repository.ts
@@ -7,11 +7,24 @@ export class ViewersRepository {
   constructor(private readonly sheetsService: SheetsService) {}
 
   async find(): Promise<Viewer[]> {
-    return [];
+    const rows = await this.sheetsService.getSheets();
+    const viewers = rows.map((row) => new Viewer(row));
+
+    return viewers;
   }
 
-  async findOne(option: Partial<Viewer> = {}): Promise<Viewer> {
-    return null;
+  async findOne(option: Partial<Viewer> = {}): Promise<Viewer | null> {
+    const rows = await this.sheetsService.getSheets();
+    const row = rows.find((row) => {
+      return Object.entries(option).every(([key, value]) => row[key] === value);
+    });
+    if (!row) {
+      return null;
+    }
+
+    const viewer = new Viewer(row);
+
+    return viewer;
   }
 
   async save(viewer: Viewer): Promise<Viewer> {
