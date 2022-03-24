354444f2cba7a60a065c9860cfc9218aa8e59662
XxshiftxX <contact@dev-shift.me>
2022-03-24 15:46:06 +0900

    find 테스트 작성

--- src/viewers/viewers.repository.spec.ts
+++ src/viewers/viewers.repository.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { SheetRow } from 'src/sheets/sheets.interface';
 import { SheetsService } from 'src/sheets/sheets.service';
+import { Viewer } from './viewers.entity';
 import { ViewersRepository } from './viewers.repository';
 
 describe('ViewersRepository', () => {
   let repository: ViewersRepository;
+  let sheetsService: SheetsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +20,57 @@ describe('ViewersRepository', () => {
     }).compile();
 
     repository = module.get<ViewersRepository>(ViewersRepository);
+    sheetsService = module.get<SheetsService>(SheetsService);
   });
 
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  describe('find', () => {
+    it('시트 정보를 가져와 Viewer 목록을 반환해야 한다', async () => {
+      sheetsService.getSheets = jest.fn(
+        async (): Promise<SheetRow[]> => [
+          {
+            index: 1,
+            ticket: 10,
+            ticketPiece: 12,
+            twitchId: 'testuser1',
+            username: '테스트유저1',
+          },
+          {
+            index: 2,
+            ticket: 6,
+            ticketPiece: 0,
+            twitchId: 'testuser2',
+            username: '테스트유저2',
+            prefix: '테스트 칭호',
+          },
+        ],
+      );
+      const expected = [
+        new Viewer({
+          index: 1,
+          ticket: 10,
+          ticketPiece: 12,
+          twitchId: 'testuser1',
+          username: '테스트유저1',
+        }),
+        new Viewer({
+          index: 2,
+          ticket: 6,
+          ticketPiece: 0,
+          twitchId: 'testuser2',
+          username: '테스트유저2',
+          prefix: '테스트 칭호',
+        }),
+      ];
+
+      const result = await repository.find();
+
+      expect(result).toMatchObject(expected);
+      expect(result[0]).toBeInstanceOf(Viewer);
+      expect(result[1]).toBeInstanceOf(Viewer);
+    });
   });
 });
