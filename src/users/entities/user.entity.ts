b0b985cb7aee94df389be9616348033c15f2dd8d
XxshiftxX <contact@dev-shift.me>
2022-03-25 20:38:14 +0900

    User 엔티티 정의

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,18 @@
-export class User {}
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column({ unique: true })
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Column()
+  password: string;
+}
