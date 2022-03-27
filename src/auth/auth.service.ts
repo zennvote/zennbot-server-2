8f51cabc5502f4b44e2d3dc37322eab88ad44c09
XxshiftxX <contact@dev-shift.me>
2022-03-27 17:49:31 +0900

    auth 모듈 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+const saltRounds = 11;
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersSservice: UsersService) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.usersSservice.findOne(username);
+    const hashed = await this.getHashedPassword(password);
+
+    if (!user || user.password !== hashed) {
+      return null;
+    }
+    const { password: _, ...result } = user;
+
+    return result;
+  }
+
+  private async getHashedPassword(password: string) {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashed = await bcrypt.hash(password, salt);
+
+    return hashed;
+  }
+}
