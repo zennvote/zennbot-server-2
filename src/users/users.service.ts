e6744d3dbe8f6d71d63ba4058d30cc9ebd98fcab
XxshiftxX <contact@dev-shift.me>
2022-03-27 17:48:29 +0900

    불필요한 파일 삭제 반영

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  async findOne(username: string): Promise<User | null> {
+    return null;
+  }
 }
