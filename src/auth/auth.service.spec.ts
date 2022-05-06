bb4774399c928920d40b47c437dc2820822fb826
XxshiftxX <contact@dev-shift.me>
2022-05-07 03:09:38 +0900

    auth.service.spec에 UsersService Provide 추가

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -6,7 +7,7 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, { provide: UsersService, useValue: {} }],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
