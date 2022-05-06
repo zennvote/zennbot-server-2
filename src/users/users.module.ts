08a8b1bede5431cd4b7704788be80e707baf13df
XxshiftxX <contact@dev-shift.me>
2022-05-07 03:09:52 +0900

    UsersService.findOne 구현

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService],
 })
 export class UsersModule {}
