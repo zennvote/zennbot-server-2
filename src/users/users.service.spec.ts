b68eafabd181cd525070af684b6189679315bf15
XxshiftxX <contact@dev-shift.me>
2022-05-07 03:01:45 +0900

    UsersService findOne 테스트 작성

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from './entities/user.entity';
+import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
+
+const getTestUser = () => {
+  const user = new User();
+  user.id = 1;
+  user.username = 'testuser1';
+  return user;
+};
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: UsersRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, UsersRepository],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    repository = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('유저를 반환해야 한다.', async () => {
+      const expected = getTestUser();
+      repository.findByUsername = jest.fn(async () => expected);
+
+      const result = await service.findOne('testuser1');
+
+      expect(result).toMatchObject(expected);
+    });
+
+    it('해당 유저가 없을 시 null을 반환해야 한다.', async () => {
+      repository.findByUsername = jest.fn(async () => null);
+
+      const result = await service.findOne('testuser1');
+
+      expect(result).toBeNull();
+    });
   });
 });
