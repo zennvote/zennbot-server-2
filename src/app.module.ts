a8c6c5261ba9441eb24131c5b985ea26df9915f0
XxshiftxX <contact@dev-shift.me>
2022-05-09 20:59:11 +0900

    JWT_SECRET을 configService에서 가져오도록 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,9 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,11 +11,18 @@ import { AuthController } from './auth.controller';
 import { AuthRepository } from './auth.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserDataModel]),
-    JwtModule.register({ secret: 'process.env.JWT_SECRET', signOptions: { expiresIn: '60m' } }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60m' },
+      }),
+      inject: [ConfigService],
+    }),
     UsersModule,
     PassportModule,
   ],
