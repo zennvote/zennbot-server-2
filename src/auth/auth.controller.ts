ed78b5c15075e2655d4c5183bb75b0afd30e01e8
XxshiftxX <contact@dev-shift.me>
2022-05-15 02:59:00 +0900

    validation endpoint 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -10,5 +11,11 @@ export class AuthController {
   @Post('login')
   async login(@Request() req: any) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('validate')
+  async validate() {
+    return { result: 'got valid token' };
   }
 }
