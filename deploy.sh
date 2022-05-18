52151b8037bf2ec980d8e4e82b26945382d32687
XxshiftxX <contact@dev-shift.me>
2022-05-18 10:11:01 +0900

    배포 후 불필요한 이미지를 제거하도록 변경

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,6 @@ cp /var/env/zennbot/* .
 cp /var/env/zennbot/.env .env
 
 docker-compose up -d --build
+
+docker rm $(docker ps --filter status=exited -q)
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
