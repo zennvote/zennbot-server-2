3930a8800ada485ed7010a948941c6ef5c81d010
XxshiftxX <contact@dev-shift.me>
2022-06-28 19:23:14 +0900

    idols controller 구현

--- src/idols/idols.entity.ts
+++ src/idols/idols.entity.ts
@@ -56,4 +56,40 @@ export class Idol {
     this.cv = initializer?.cv;
     this.introduction = initializer?.introduction;
   }
+
+  get stringified() {
+    const name = this.firstName ? `${this.firstName} ${this.lastName}` : this.lastName;
+    const idolInfo = [
+      parseInformation(this.company, '소속사 불명', ' 프로덕션'),
+      parseInformation(this.unit, null, ' 소속'),
+      parseInformation(this.type, null, '타입'),
+    ]
+      .filter((value) => value)
+      .join(', ');
+    const birthday = parseInformation(this.birthday, '생일 불명', '생');
+    const age = parseInformation(this.age, '나이 불명', '세');
+    const bodyInfo = [
+      parseInformation(this.height, '신장 불명', 'cm'),
+      parseInformation(this.weight, '체중 불명', 'kg'),
+      parseInformation(this.threeSize, null),
+    ]
+      .filter((value) => value)
+      .join(', ');
+    const hometown = parseInformation(this.hometown, '출신지 불명', ' 출신');
+    const cv = parseInformation(this.cv, null, '', 'CV. ');
+
+    return [name, idolInfo, birthday, age, bodyInfo, hometown, cv, this.introduction]
+      .filter((value) => value)
+      .join(' / ');
+  }
 }
+
+const parseInformation = (value: string | undefined, invalid: string | null, prefix = '', suffix = '') => {
+  if (!value) {
+    return undefined;
+  }
+  if (value === '불명') {
+    return invalid ? invalid : undefined;
+  }
+  return suffix + value + prefix;
+};
