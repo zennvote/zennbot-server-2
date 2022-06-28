8416bb700c1142f7b795d01306ac3edf416d2390
XxshiftxX <contact@dev-shift.me>
2022-06-28 13:44:14 +0900

    RefreshToken의 secure option을 프로덕션에서만 활성화하도록 변경

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,11 @@ export class AuthController {
   async login(@Request() req: any, @Response() res: ResponseType) {
     const { access_token, refreshToken } = await this.authService.login(req.user);
 
-    res.cookie('RefreshToken', refreshToken, { httpOnly: true, secure: true, domain: process.env.COOKIE_DOMAIN });
+    res.cookie('RefreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      domain: process.env.COOKIE_DOMAIN,
+    });
 
     return res.json({ access_token });
   }
