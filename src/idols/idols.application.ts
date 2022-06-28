898f7d97e42495cfe36720d1c14604f93a4476c5
XxshiftxX <contact@dev-shift.me>
2022-06-28 18:26:24 +0900

    idols application 구현

--- src/idols/idols.application.ts
+++ src/idols/idols.application.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { BusinessError } from 'src/util/business-error';
 import { IdolsService } from './idols.service';
 
 @Injectable()
 export class IdolsApplication {
   constructor(private readonly idolsService: IdolsService) {}
 
-  async searchIdol(keyword: string) {}
+  async searchIdol(keyword: string) {
+    const result = await this.idolsService.searchIdols(keyword);
+
+    if (result.length > 1) {
+      return new BusinessError('multiple-result');
+    }
+    if (result.length === 0) {
+      return new BusinessError('no-result');
+    }
+
+    return result[0];
+  }
 }
