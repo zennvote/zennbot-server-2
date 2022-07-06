6d67afbcc566574a50e95e4da48f70a7ecbe43b0
XxshiftxX <contact@dev-shift.me>
2022-07-06 10:22:32 +0900

    hotfix: db migration을 위해 ormconfig 추가

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,23 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+const config: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5433', 10) ?? 5433,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  synchronize: false,
+  logging: true,
+  entities: ['dist/**/*{.entity.js,.entity.ts}', 'dist/**/*{.datamodel.js,.datamodel.ts}'],
+  migrations: ['dist/src/migrations/*{.js,.ts}'],
+  subscribers: ['dist/src/subscribers/*{.js,.ts}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
+
 export const getConfigWithConfigService = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: configService.get('DB_HOST'),
@@ -17,3 +34,5 @@ export const getConfigWithConfigService = (configService: ConfigService): TypeOr
     migrationsDir: 'src/migrations',
   },
 });
+
+export default config;
