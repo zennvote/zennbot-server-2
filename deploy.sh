43a638c4db7bf92862da04efef9367941fde8a3d
XxshiftxX <contact@dev-shift.me>
2022-07-15 09:58:16 +0900

    배포 방식을 ECS에서 CodeDeploy로 변경

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cp /var/env/zennbot/* .
-cp /var/env/zennbot/.env .env
+docker stop zennbot-web
+docker rm zennbot-web
 
-docker-compose up -d --build
+docker pull 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web:latest
+docker run -d --name zennbot-web 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
 
 docker rm $(docker ps --filter status=exited -q)
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
