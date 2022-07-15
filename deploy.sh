f7a6d4189bfa2432e185078a2f139fdd7c1271ba
XxshiftxX <contact@dev-shift.me>
2022-07-15 13:12:24 +0900

    컨테이너 실행 시 포트 추가

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ docker rm zennbot-web
 
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web:latest
-docker run -d --name zennbot-web 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
+docker run -d --name zennbot-web -p 3000:3000 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
 
 docker rm $(docker ps --filter status=exited -q)
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
