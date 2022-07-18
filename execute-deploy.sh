a1e7804867c65b708611c072ac133b6716f6dc54
XxshiftxX <contact@dev-shift.me>
2022-07-18 13:53:10 +0900

    실패 가능성이 있는 명령어에 || true 추가

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker stop zennbot-web
-docker rm zennbot-web
+docker stop zennbot-web || true
+docker rm zennbot-web || true
 
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web:latest
 docker run -d --name zennbot-web -p 3000:3000 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
 
-docker rm $(docker ps --filter status=exited -q)
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+docker rm $(docker ps --filter status=exited -q) || true
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc) || true
