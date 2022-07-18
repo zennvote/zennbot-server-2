710b68f686f866b6b4490991c092ef1569538bff
XxshiftxX <contact@dev-shift.me>
2022-07-18 16:46:35 +0900

    typeorm 코드 제거

--- src/app/managers/managers.entity.ts
+++ src/app/managers/managers.entity.ts
@@ -1,16 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
 export class Manager {
-  @PrimaryGeneratedColumn()
   @ApiProperty()
   id!: number;
 
   @IsString()
   @IsNotEmpty()
-  @Column({ unique: true })
   @ApiProperty()
   twitchId!: string;
 }
