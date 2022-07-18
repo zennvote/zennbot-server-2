8fd2b7e02d0c825fcac4e90a377bc3a7ff468fa3
XxshiftxX <contact@dev-shift.me>
2022-07-18 13:30:09 +0900

    ecr login 수정

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -2,10 +2,8 @@
 
 cd /home/ec2-user/zennbot-deploy
 
-docker stop zennbot-web
-docker rm zennbot-web
-
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com
+$(aws ecr get-login --region ap-northeast-2 --no-include-email)
+docker rm -f zennbot-web
 docker pull 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web:latest
 docker run -d --name zennbot-web -p 3000:3000 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
 
