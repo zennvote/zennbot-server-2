c959e3e3fdb33fc23b5d758d65c764b40edb6f57
XxshiftxX <contact@dev-shift.me>
2022-07-18 11:57:37 +0900

    execute-deploy 파일에서 바로 배포하도록 변경

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cd /home/ec2-user/zennbot-deploy
-./deploy.sh > /dev/null 2> /dev/null < /dev/null &
+docker stop zennbot-web
+docker rm zennbot-web
+
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com
+docker pull 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web:latest
+docker run -d --name zennbot-web -p 3000:3000 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
+
+docker rm $(docker ps --filter status=exited -q)
+docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
