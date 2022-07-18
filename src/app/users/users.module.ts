67f7d1ea2e9d12c21d8b2ab17020d4253e67c637
XxshiftxX <contact@dev-shift.me>
2022-07-18 16:41:45 +0900

    typeorm moduel 제거

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,13 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/app/auth/auth.module';
-import { UserDataModel } from './entities/user.datamodel';
 import { UsersRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserDataModel]), forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule)],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
   controllers: [UsersController],
