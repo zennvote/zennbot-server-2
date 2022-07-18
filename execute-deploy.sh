db8058062dd8b789708adaf9ada0714853ed6230
XxshiftxX <contact@dev-shift.me>
2022-07-18 13:37:58 +0900

    IAM 권한 추가 후 shell 복구

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/ec2-user/zennbot-deploy
+docker stop zennbot-web
+docker rm zennbot-web
 
-$(aws ecr get-login --region ap-northeast-2 --no-include-email)
-docker rm -f zennbot-web
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web:latest
 docker run -d --name zennbot-web -p 3000:3000 998737244507.dkr.ecr.ap-northeast-2.amazonaws.com/zennbot-web
 
