3c73ebbfb8ee6786a8d5b649ab5a2e084ad0afdf
XxshiftxX <contact@dev-shift.me>
2022-07-25 16:41:41 +0900

    getSubscription 함수 추가

---
+++ src/util/twitch.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const twitchGqlUrl = 'https://gql.twitch.tv/gql';
+
+const headers = {
+  Authorization: `OAuth ${process.env.TWITCH_GQL_CLIENT_AUTH}`,
+  'Client-Id': process.env.TWITCH_GQL_CLIENT_ID ?? '',
+};
+
+export const getSubscription = async (channel: string, channelId: string, twitchId: string) => {
+  const {
+    data: [{ data }],
+  } = await axios.post(
+    twitchGqlUrl,
+    [
+      {
+        operationName: 'ViewerCard',
+        variables: {
+          channelID: channelId,
+          channelLogin: channel,
+          hasChannelID: true,
+          giftRecipientLogin: twitchId,
+          isViewerBadgeCollectionEnabled: false,
+          withStandardGifting: false,
+        },
+        extensions: {
+          persistedQuery: {
+            version: 1,
+            sha256Hash: '9afddab81b8b216f9370f3f96662d4cefe9eb5312dc4c133ace70fa0a24ec2af',
+          },
+        },
+      },
+    ],
+    { headers },
+  );
+
+  const { tier: tierString } = data?.targetUser?.relationship?.subscriptionBenefit ?? {};
+  const tier = parseInt(tierString, 10);
+
+  if (tier >= 3000) {
+    return 3;
+  }
+  if (tier >= 2000) {
+    return 2;
+  }
+
+  return 1;
+};
