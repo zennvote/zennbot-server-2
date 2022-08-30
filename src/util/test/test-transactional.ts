8509c695c61858abaff3f8dcfa621ced1262b821
BuildTools <unconfigured@null.spigotmc.org>
2022-08-30 16:18:28 +0900

    transactionalTest이 인자로 받은 PrismaClient를 transaction으로 덮어쓰도록 변경

--- src/util/test/test-transactional.ts
+++ src/util/test/test-transactional.ts
@@ -1,11 +1,19 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient({ });
+
 export const transactionalTest = async (
-  prisma: PrismaClient,
+  targetPrisma: PrismaClient,
   fn: (transaction: Prisma.TransactionClient) => Promise<unknown>,
 ) => {
   try {
     await prisma.$transaction(async (transaction) => {
+      const mockTargetKeys = Object.keys(transaction).filter((key) => !key.startsWith('_'));
+      mockTargetKeys.forEach((key) => {
+        // eslint-disable-next-line no-param-reassign
+        (targetPrisma as any)[key] = transaction[key];
+      });
+
       await fn(transaction);
 
       throw new Error('rollback transaction');
