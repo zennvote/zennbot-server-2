6a74f6ddda22cb0d2d68f465105ab89250b9f073
BuildTools <unconfigured@null.spigotmc.org>
2022-09-01 17:49:04 +0900

    로그를 별도 디렉토리에 저장하도록 변경

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -11,8 +11,8 @@ export class MainLogger extends ConsoleLogger {
     format: winston.format.json(),
     defaultMeta: { service: 'user-service' },
     transports: [
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log', level: 'debug' }),
+      new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'logs/combined.log', level: 'debug' }),
     ],
   });
 
