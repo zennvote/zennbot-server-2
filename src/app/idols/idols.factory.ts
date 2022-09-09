634a7a2f8d1bb163597756d754d2ea36e5668c2a
BuildTools <unconfigured@null.spigotmc.org>
2022-09-09 09:32:07 +0900

    테스트용 Factory 추가

---
+++ src/app/idols/idols.factory.ts
@@ -0,0 +1,25 @@
+import { Factory } from 'fishery';
+import { Idol } from './idols.entity';
+
+class IdolFactory extends Factory<Idol> {
+}
+
+export const idolFactory = IdolFactory.define(
+  ({ params }) => new Idol({ ...params }),
+);
+
+export const getRowFromIdol = (idol: Idol): (string | undefined)[] => [
+  idol.firstName,
+  idol.lastName,
+  idol.company,
+  idol.unit,
+  idol.type,
+  idol.birthday,
+  idol.age,
+  idol.height,
+  idol.weight,
+  idol.threeSize,
+  idol.hometown,
+  idol.cv,
+  idol.introduction,
+];
