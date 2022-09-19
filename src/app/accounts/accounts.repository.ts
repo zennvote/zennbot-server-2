873786d14f5a5ce598cc51541f8f8438321b9601
BuildTools <unconfigured@null.spigotmc.org>
2022-09-19 19:33:34 +0900

    findAccount 메서드 추가

--- src/app/accounts/accounts.repository.ts
+++ src/app/accounts/accounts.repository.ts
@@ -3,6 +3,15 @@ import { SheetsService } from 'src/libs/sheets/sheets.service';
 import { Account, AccountCreateParams } from './entities/account.entity';
 import { AccountCreatedEvent } from './events/account-created.event';
 
+type ViewerRow = {
+  index: number;
+  username?: string;
+  twitchId?: string;
+  ticket?: string;
+  ticketPiece?: string;
+  prefix?: string;
+};
+
 @Injectable()
 export class AccountsRepository {
   private readonly sheetsInfo = {
@@ -13,6 +22,22 @@ export class AccountsRepository {
 
   constructor(private readonly sheetsService: SheetsService) {}
 
+  async findAccount(username: string, twitchId?: string) {
+    const rows = await this.sheetsService.getSheets(this.sheetsInfo);
+
+    const row = rows.find((row) => row.username === username || row.twitchId === twitchId);
+    if (!row) {
+      return null;
+    }
+    if (row.username !== username || row.twitchId !== twitchId) {
+      await this.sheetsService.updateSheets(this.sheetsInfo, row.index, { twitchId, username });
+    }
+
+    const account = AccountsRepository.rowToViewer(row);
+
+    return account;
+  }
+
   async create(accounts: Omit<AccountCreateParams, 'id'>) {
     const id = await this.sheetsService.appendRow(this.sheetsInfo, accounts);
     const account = new Account({ ...accounts, id });
@@ -20,5 +45,20 @@ export class AccountsRepository {
     account.apply(new AccountCreatedEvent(id));
 
     return account;
+  }
+
+  private static rowToViewer(row: ViewerRow) {
+    if (!row.username) {
+      return null;
+    }
+
+    return new Account({
+      id: row.index,
+      username: row.username,
+      twitchId: row.twitchId,
+      ticket: Number.parseInt(row.ticket ?? '0', 10),
+      ticketPiece: Number.parseInt(row.ticketPiece ?? '0', 10),
+      prefix: row.prefix,
+    });
   }
 }
