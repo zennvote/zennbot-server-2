5616c8ab344251591aecda99f19fbe46eb960abc
BuildTools <unconfigured@null.spigotmc.org>
2022-09-23 10:48:27 +0900

    Account 도메인 이전 및 이벤트 정의

---
+++ src/domain/accounts/events/index.ts
@@ -0,0 +1,9 @@
+import { DepositedEvent } from './deposited.event';
+import { RegisteredEvent } from './registered.event';
+import { WithdrawnEvent } from './withdrawn.event';
+
+export const AccountEvents = {
+  DepositedEvent,
+  RegisteredEvent,
+  WithdrawnEvent,
+};
