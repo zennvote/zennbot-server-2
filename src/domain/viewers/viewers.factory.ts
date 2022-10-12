623fe6e849315d8636e701af0c1dd828cf2cd55c
BuildTools <unconfigured@null.spigotmc.org>
2022-10-13 00:07:30 +0900

    테스트 코드 추가

---
+++ src/domain/viewers/viewers.factory.ts
@@ -0,0 +1,16 @@
+import { Factory } from 'fishery';
+import { Viewer, ViewerProps } from 'src/domain/viewers/viewers.entity';
+
+export const viewerFactory = Factory.define<ViewerProps, Record<string, never>, Viewer>(
+  ({ params, sequence, onCreate }) => {
+    onCreate((props) => new Viewer(props));
+
+    return {
+      id: sequence,
+      accountId: -1,
+      twitchId: `테스트시청자${sequence}`,
+      username: `testviewer${sequence}`,
+      ...params,
+    };
+  },
+);
