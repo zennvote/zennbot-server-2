3c2e23f80e2f1568d581be1b24b60bacb05052b4
BuildTools <unconfigured@null.spigotmc.org>
2022-10-18 20:23:10 +0900

    신청곡 신청 유즈케이스에 설정 관련 로직 추가

--- src/application/songs/songs.application.ts
+++ src/application/songs/songs.application.ts
@@ -5,22 +5,31 @@ import { AccountsRepository } from 'src/infrastructure/persistence/accounts/acco
 import { SongsRepository } from 'src/infrastructure/persistence/songs/songs.repository';
 import { ViewersRepository } from 'src/infrastructure/persistence/viewers/viewers.repository';
 
+import * as Settings from 'src/domain/settings/settings-store';
 import { SongsService } from 'src/domain/songs/songs.service';
 import { RequestType } from 'src/domain/songs/songs.entity';
+import { SettingsRepository } from 'src/infrastructure/persistence/settings/settings.repository';
+import { SettingsService } from 'src/domain/settings/settings.service';
 
 @Injectable()
 export class SongsApplication {
   private readonly songsService: SongsService;
+  private readonly settingsService: SettingsService;
 
   constructor(
     private readonly viewersRepository: ViewersRepository,
     private readonly accountsRepository: AccountsRepository,
     private readonly songsRepository: SongsRepository,
+    settingsRepository: SettingsRepository,
   ) {
     this.songsService = new SongsService(songsRepository);
+    this.settingsService = new SettingsService(settingsRepository);
   }
 
   public async requestSong(twitchId: string, username: string, title: string) {
+    const isRequestEnabled = this.settingsService.getSetting(Settings.IsRequestEnabled);
+    if (!isRequestEnabled) return new BusinessError('request-not-enabled');
+
     const viewer = await this.viewersRepository.findOne(twitchId);
     if (viewer === null) return new BusinessError('no-viewer');
 
@@ -30,7 +39,7 @@ export class SongsApplication {
     const paymentResult = account.payForRequestSong();
     if (isBusinessError(paymentResult)) return paymentResult;
 
-    const requestType = paymentResult === 'ticket' ? RequestType.ticket : RequestType.ticketPiece;
+    const requestType = await this.getRequestType(paymentResult);
     const song = await viewer.requestSong(title, requestType, this.songsService);
     if (isBusinessError(song)) return song;
 
@@ -40,5 +49,15 @@ export class SongsApplication {
     ]);
 
     return persisted;
+  }
+
+  private async getRequestType(paymentResult: 'ticket' | 'ticketPiece') {
+    const isGoldenbellEnabled = await this.settingsService.getSetting(Settings.IsGoldenbellEnabled);
+
+    if (isGoldenbellEnabled) return RequestType.freemode;
+    if (paymentResult === 'ticket') return RequestType.ticket;
+    if (paymentResult === 'ticketPiece') return RequestType.ticketPiece;
+
+    throw new Error('invalid payment result');
   }
 }
---
+++ src/infrastructure/persistence/settings/settings.repository.ts
@@ -0,0 +1,14 @@
+import { SettingTypeMap } from 'src/domain/settings/settings-store';
+import { SettingsRepository as SettingsRepositoryInterface } from 'src/domain/settings/settings.repository';
+
+export class SettingsRepository implements SettingsRepositoryInterface {
+  getSetting<Key extends keyof SettingTypeMap>(key: Key): Promise<SettingTypeMap[Key]> {
+    throw new Error('Method not implemented.');
+  }
+  setSetting<Key extends keyof SettingTypeMap>(
+    key: Key,
+    value: SettingTypeMap[Key],
+  ): Promise<SettingTypeMap[Key]> {
+    throw new Error('Method not implemented.');
+  }
+}
