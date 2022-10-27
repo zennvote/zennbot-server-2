29c681fecfc55b8f12990d749ee78f673411335a
BuildTools <unconfigured@null.spigotmc.org>
2022-10-27 18:17:42 +0900

    account 엔티티를 viewer와 통합

--- src/domain/viewers/viewers.entity.ts
+++ src/domain/viewers/viewers.entity.ts
@@ -4,23 +4,40 @@ import { RequestType, Song } from 'src/domain/songs/songs.entity';
 import { SongsService } from 'src/domain/songs/songs.service';
 import { Entity, EntityProps } from 'src/domain/types/entity';
 
-const constructorKey = ['id', 'twitchId', 'username', 'accountId', 'viasIdolIds'] as const;
-export type ViewerProps = EntityProps<Viewer, typeof constructorKey>;
+const requiredKey = ['id', 'username', 'ticket', 'ticketPiece', 'viasIdolIds'] as const;
+const optionalKey = ['twitchId', 'prefix'] as const;
+
+const constructorKey = [...requiredKey, ...optionalKey] as const;
+export type ViewerProps = EntityProps<Viewer, typeof requiredKey, typeof optionalKey>;
 
 export class Viewer extends Entity {
   public readonly id!: number;
-  public readonly twitchId!: string;
+  public readonly twitchId?: string;
   public readonly username!: string;
-  public readonly accountId!: number;
+  public readonly ticket!: number;
+  public readonly ticketPiece!: number;
+  public readonly prefix?: string;
   public readonly viasIdolIds!: number[];
 
   constructor(props: ViewerProps) {
     super(props, constructorKey);
   }
 
-  public async requestSong(title: string, requestType: RequestType, songsService: SongsService) {
+  public async requestSong(title: string, songsService: SongsService) {
     const isInCooltime = await songsService.isViewerInCooltime(this);
     if (isInCooltime) return new BusinessError('in-cooltime');
+
+    if (this.ticket < 1 && this.ticketPiece < 3) return new BusinessError('not-enough-point');
+
+    const requestType = this.ticket > 0 ? RequestType.ticket : RequestType.ticketPiece;
+    switch (requestType) {
+      case RequestType.ticket:
+        this.mutable.ticket -= 1;
+        break;
+      case RequestType.ticketPiece:
+        this.mutable.ticketPiece -= 3;
+        break;
+    }
 
     return new Song({
       id: -1,
