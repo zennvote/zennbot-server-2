982bf3a186ae982c516d16f91fce940b2acca551
XxshiftxX <qjfrntop12@gmail.com>
2023-02-27 18:47:28 +0900

    도메인 코드 작성

---
+++ src/domain/songs/entities/song-queue.entity.ts
@@ -0,0 +1,69 @@
+import { randomUUID } from 'crypto';
+
+import { BusinessError, isBusinessError } from 'src/util/business-error';
+
+import { Entity } from 'src/domain/types/entity';
+
+import { SongRequestor } from './song-requestor.entity';
+import { RequestType, Song } from './songs.entity';
+
+export type SongQueueProps = {
+  id: string;
+  requestedSongs: Song[];
+  consumedSongs: Song[];
+  isRequestEnabled: boolean;
+  isGoldenBellEnabled: boolean
+};
+
+export class SongQueue extends Entity {
+  public readonly requestedSongs: Song[];
+  public readonly consumedSongs: Song[];
+  public readonly isRequestEnabled: boolean;
+  public readonly isGoldenBellEnabled: boolean;
+
+  constructor(props: SongQueueProps) {
+    super(props.id);
+
+    this.requestedSongs = props.requestedSongs;
+    this.consumedSongs = props.consumedSongs;
+    this.isRequestEnabled = props.isRequestEnabled;
+    this.isGoldenBellEnabled = props.isGoldenBellEnabled;
+  }
+
+  consume() {
+    const consumed = this.requestedSongs.shift();
+    if (!consumed) return new BusinessError('empty-queue');
+
+    this.consumedSongs.push(consumed);
+    if (this.consumedSongs.length > 4) {
+      this.consumedSongs.shift();
+    }
+  }
+
+  request(title: string, requestor: SongRequestor) {
+    if (this.isRequestEnabled === false) return new BusinessError('request-disabled');
+
+    const requestType = this.payForRequestAndGetRequestType(requestor);
+    if (isBusinessError(requestType)) return requestType;
+
+    const song = new Song({
+      id: randomUUID(),
+      title,
+      requestorId: requestor.twitchId,
+      requestType,
+    });
+
+    this.requestedSongs.push(song);
+
+    return song;
+  }
+
+  payForRequestAndGetRequestType(requestor: SongRequestor) {
+    if (this.isGoldenBellEnabled) return RequestType.freemode;
+
+    const payment = requestor.payForRequest();
+    if (isBusinessError(payment)) return payment;
+
+    return payment;
+  }
+}
---
+++ src/domain/songs/entities/song-requestor.entity.ts
@@ -0,0 +1,41 @@
+import { BusinessError } from 'src/util/business-error';
+
+import { Entity } from 'src/domain/types/entity';
+
+import { RequestType } from './songs.entity';
+
+export type SongRequestorProps = {
+  id: string;
+  username: string;
+  twitchId: string;
+  ticket: number;
+  ticketPiece: number;
+};
+
+export class SongRequestor extends Entity {
+  public readonly username: string;
+  public readonly twitchId: string;
+  public readonly ticket: number;
+  public readonly ticketPiece: number;
+
+  constructor(props: SongRequestorProps) {
+    super(props.id);
+
+    this.username = props.username;
+    this.twitchId = props.twitchId;
+    this.ticket = props.ticket;
+    this.ticketPiece = props.ticketPiece;
+  }
+
+  payForRequest() {
+    if (this.ticket >= 1) {
+      this.mutable.ticket -= 1;
+      return RequestType.ticket;
+    }
+    if (this.ticketPiece >= 3) {
+      this.mutable.ticketPiece -= 3;
+      return RequestType.ticketPiece;
+    }
+    return new BusinessError('not-enough-point');
+  }
+}
