12385ee3c1c43d0ee9be85f047e1f0c9d5ac0304
XxshiftxX <qjfrntop12@gmail.com>
2023-02-28 18:28:12 +0900

    추가된 entity의 팩토리 추가

--- src/domain/songs/songs.factory.ts
+++ src/domain/songs/songs.factory.ts
@@ -2,9 +2,13 @@ import { randomUUID } from 'crypto';
 
 import { Factory } from 'fishery';
 
+import { SongQueue, SongQueueProps } from './entities/song-queue.entity';
+import { SongRequestor, SongRequestorProps } from './entities/song-requestor.entity';
 import { RequestType, Song, SongProps } from './entities/songs.entity';
 
-export const songFactory = Factory.define<SongProps, Record<string, never>, Song>(
+type Blank = Record<string, never>;
+
+export const songFactory = Factory.define<SongProps, Blank, Song>(
   ({ params, sequence, onCreate }) => {
     onCreate((props) => new Song(props));
 
@@ -13,6 +17,36 @@ export const songFactory = Factory.define<SongProps, Record<string, never>, Song
       title: `test song ${sequence}`,
       requestorId: randomUUID(),
       requestType: RequestType.manual,
+      ...params,
+    };
+  },
+);
+
+export const songQueueFactory = Factory.define<SongQueueProps, Blank, SongQueue>(
+  ({ params, onCreate }) => {
+    onCreate((props) => new SongQueue(props));
+
+    return {
+      id: randomUUID(),
+      requestedSongs: [],
+      consumedSongs: [],
+      isRequestEnabled: true,
+      isGoldenBellEnabled: false,
+      ...params,
+    };
+  },
+);
+
+export const songRequestorFactory = Factory.define<SongRequestorProps, Blank, SongRequestor>(
+  ({ params, sequence, onCreate }) => {
+    onCreate((props) => new SongRequestor(props));
+
+    return {
+      id: randomUUID(),
+      twitchId: `testuser${sequence}`,
+      username: `유저 ${sequence}`,
+      ticket: 0,
+      ticketPiece: 0,
       ...params,
     };
   },
