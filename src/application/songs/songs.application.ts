c8ae4a029d06cdc8d4a63c78290b08bd94bfffa8
XxshiftxX <qjfrntop12@gmail.com>
2023-02-28 18:28:31 +0900

    스펙 추가 및 스펙에 맞게 코드 수정

--- src/application/songs/songs.application.ts
+++ src/application/songs/songs.application.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 
-import { isBusinessError } from 'src/util/business-error';
+import { BusinessError, isBusinessError } from 'src/util/business-error';
 
 import { SONG_QUEUE_REPOSITORY, SongQueueRepository } from 'src/domain/songs/repositories/song-queue.repository';
 import { SONG_REQUESTOR_REPOSITORY, SongRequestorRepository } from 'src/domain/songs/repositories/song-requestor.repository';
@@ -13,14 +13,14 @@ export class SongsApplication {
     @Inject(SONG_QUEUE_REPOSITORY) private readonly songQueueRepository: SongQueueRepository,
     @Inject(SONG_REQUESTOR_REPOSITORY)
     private readonly songRequestorRepository: SongRequestorRepository,
-  ) {
-  }
+  ) {}
 
   public async requestSong(twitchId: string, username: string, title: string) {
     const songQueue = await this.songQueueRepository.get();
-    const requestor = await this.songRequestorRepository.get(username, twitchId);
+    const requestor = await this.songRequestorRepository.get(twitchId, username);
+    if (requestor === null) return new BusinessError('requestor-not-found');
 
-    const song = songQueue.request(title, requestor);
+    const song = songQueue.request(title, twitchId, requestor);
     if (isBusinessError(song)) return song;
 
     const [persisted] = await Promise.all([
@@ -30,5 +30,22 @@ export class SongsApplication {
     ]);
 
     return persisted;
+  }
+
+  public async consumeSong() {
+    const songQueue = await this.songQueueRepository.get();
+    const song = songQueue.consume();
+    if (isBusinessError(song)) return song;
+
+    const [persisted] = await Promise.all([
+      await this.songsRepository.save(song),
+      await this.songQueueRepository.save(songQueue),
+    ]);
+
+    return persisted;
+  }
+
+  public async getQueue() {
+    return this.songQueueRepository.get();
   }
 }
