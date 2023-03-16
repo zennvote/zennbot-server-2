0736a274f0391ffb2768f289e0da495e1113085d
XxshiftxX <qjfrntop12@gmail.com>
2023-03-16 14:37:32 +0900

    twitch api 크론 추가

---
+++ src/app/attendances/attendances.cron.ts
@@ -0,0 +1,40 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { AxiosError } from 'axios';
+
+import * as twitch from 'src/util/twitch';
+
+@Injectable()
+export class AttendancesTaskService {
+  private readonly logger = new Logger(AttendancesTaskService.name);
+
+  constructor(
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Cron(CronExpression.EVERY_DAY_AT_10AM)
+  async checkSubscriptionApi() {
+    this.logger.log('Check twitch subscription api');
+
+    const channel = this.configService.get('TMI_CHANNEL');
+    const channelId = this.configService.get('TMI_CHANNEL_ID');
+
+    try {
+      const subscription = await twitch.getSubscription(channel, channelId, 'producerzenn');
+      if (subscription !== 1 && subscription !== 2 && subscription !== 3) {
+        this.logger.error(`twitch subscription api returned invalid value: ${subscription}`);
+        return;
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        this.logger.error(`twitch subscription api failed on fetch witch status: ${error.status}`);
+        this.logger.error(error.response?.data);
+
+        return;
+      }
+      this.logger.error('twitch subscription api failed with unknown error');
+      this.logger.error(error);
+    }
+  }
+}
