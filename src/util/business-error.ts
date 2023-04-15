3f31d173f08fdc952e21237448971ab85de85b09
XxshiftxX <qjfrntop12@gmail.com>
2023-04-16 05:04:31 +0900

    mapError 유틸 함수 추가

--- src/util/business-error.ts
+++ src/util/business-error.ts
@@ -7,4 +7,8 @@ export const isBusinessError = (
 ): value is BusinessError<string> => value instanceof BusinessError;
 
 export type Error<T> = T extends BusinessError<infer U> ? U : never;
-export type ErrorMap<T, R = string> = { [key in Error<T>]: R }
+export type ErrorMap<T, R = string> = { [key in Error<T>]: R };
+export const mapError = <T, R = string>(error: T, errorMap: ErrorMap<T, R>) => {
+  if (isBusinessError(error)) return errorMap[error.error];
+  return error;
+};
