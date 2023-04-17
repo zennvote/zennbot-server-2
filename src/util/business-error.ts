c584e47a8681cb8faa73f0ef4ee32c9a181c81c5
XxshiftxX <qjfrntop12@gmail.com>
2023-04-17 16:46:24 +0900

    🎨 mapError에 에러 throw 기능 추가

--- src/util/business-error.ts
+++ src/util/business-error.ts
@@ -6,9 +6,12 @@ export const isBusinessError = (
   value: BusinessError<string> | any,
 ): value is BusinessError<string> => value instanceof BusinessError;
 
-export type Error<T> = T extends BusinessError<infer U> ? U : never;
-export type ErrorMap<T, R = string> = { [key in Error<T>]: R };
+export type Errorable<T> = T extends BusinessError<infer U> ? U : never;
+export type ErrorMap<T, R = string> = { [key in Errorable<T>]: R | Error };
 export const mapError = <T, R = string>(error: T, errorMap: ErrorMap<T, R>) => {
-  if (isBusinessError(error)) return errorMap[error.error];
+  if (isBusinessError(error)) {
+    if (errorMap[error.error] instanceof Error) throw errorMap[error.error];
+    return errorMap[error.error];
+  }
   return error;
 };
