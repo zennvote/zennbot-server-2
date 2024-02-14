6c430d7cbb403ddbb7601620824843607fb09955
XxshiftxX <qjfrntop12@gmail.com>
2024-02-14 09:04:20 +0900

    TmiService 리팩터

--- src/libs/tmi/tmi.service.ts
+++ src/libs/tmi/tmi.service.ts
@@ -4,69 +4,76 @@ import { Client } from 'tmi.js';
 
 import { MainLogger } from 'src/util/logger';
 
-import { TMI_CLIENT } from './tmi.types';
+import { CommandPayload, TMI_CLIENT } from './tmi.types';
 
 @Injectable()
 export class TmiService {
   private readonly logger = new MainLogger('TmiLogger');
 
   constructor(@Inject(TMI_CLIENT) private client: Client, private eventEmitter: EventEmitter2) {
-    client.on('message', (channel, tags, fullMessage, self) => {
-      if (self) {
-        return;
-      }
-
-      const sendAt = tags['tmi-sent-ts'] ? new Date(parseInt(tags['tmi-sent-ts'], 10)) : new Date();
-      const send = (message: string) => this.client.say(channel, message);
-
-      if (fullMessage.startsWith('!') || fullMessage.startsWith('=')) {
-        const message = fullMessage.startsWith('!젠 ') || fullMessage.startsWith('=젠 ') ? fullMessage.replace('젠 ', '') : fullMessage;
-
-        const [unformattedCommand, ...args] = message.split(' ');
-        const command = unformattedCommand.slice(1).toLowerCase();
-
-        const loggingSend = (response: string) => {
-          this.logger.http(`TMI Command - ${command}`, {
-            response,
-            channel,
-            tags,
-            args,
-            message,
-            displayName: tags['display-name'],
-            username: tags.username,
-            userId: tags['user-id'],
-          });
-          send(response);
-        };
-
-        this.eventEmitter.emit(`command.${command}`, {
-          channel,
-          tags,
-          args,
-          message,
-          send: loggingSend,
-        });
-      }
-
-      if (tags.subscriber) {
-        this.logger.http('TMI Subscribe Chat', {
-          channel,
-          tags,
-          displayName: tags['display-name'],
-          username: tags.username,
-          userId: tags['user-id'],
-        });
-        this.eventEmitter.emit('subscriber-chat', {
-          twitchId: tags.username,
-          username: tags['display-name'],
-          attendedAt: sendAt,
-          send,
-        });
-      }
-    });
+    this.logChat();
+    this.handleCommand();
+    this.handleSubscriberChat();
   }
 
   sendMessaage(channel: string, message: string) {
     this.client.say(channel, message);
+  }
+
+  private logChat() {
+    this.client.on('message', (channel, tags, message, self) => {
+      if (self) return;
+
+      this.logger.http(`Chat #${tags.id}`, {
+        channel,
+        tags,
+        message,
+        displayName: tags['display-name'],
+        username: tags.username,
+        userId: tags['user-id'],
+      });
+    });
+  }
+
+  private handleCommand() {
+    this.client.on('message', (channel, tags, message, self) => {
+      if (self) return;
+      if (!message.startsWith('!')) return;
+
+      const send = (message: string) => this.client.say(channel, message);
+      if (message.startsWith('!젠 ')) {
+        // eslint-disable-next-line no-param-reassign
+        message = message.replace('젠 ', '');
+      }
+
+      const command = message.split(' ')[0].slice(1).toLowerCase();
+      const [, ...args] = message.split(' ');
+      const payload: CommandPayload = {
+        channel,
+        tags,
+        message,
+        args,
+        send,
+      };
+
+      this.eventEmitter.emit(`command.${command}`, payload);
+    });
+  }
+
+  private handleSubscriberChat() {
+    this.client.on('message', (channel, tags, message, self) => {
+      if (self) return;
+      if (!tags.subscriber) return;
+
+      const sentAt = tags['tmi-sent-ts'] ? new Date(parseInt(tags['tmi-sent-ts'], 10)) : new Date();
+      const send = (message: string) => this.client.say(channel, message);
+
+      this.eventEmitter.emit('subscriber-chat', {
+        twitchId: tags.username,
+        username: tags['display-name'],
+        attendedAt: sentAt,
+        send,
+      });
+    });
   }
 }
