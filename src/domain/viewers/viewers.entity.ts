a7a5bbe852628e516e22035b7908fd962eb58a9c
XxshiftxX <qjfrntop12@gmail.com>
2024-03-04 09:44:47 +0900

    :sparkles: 치지직 마이그레이션 어플리케이션 구현

--- src/domain/viewers/viewers.entity.ts
+++ src/domain/viewers/viewers.entity.ts
@@ -1,9 +1,8 @@
-import { randomUUID } from 'crypto';
-
 import { BusinessError } from 'src/util/business-error';
 
-import { RequestType, Song } from 'src/domain/songs/entities/songs.entity';
 import { Entity } from 'src/domain/types/entity';
+
+import { ViewerChzzkMigrationRequest } from './viewer-chzzk-migration-request.entity';
 
 export type ViewerProps = {
   id: string;
@@ -40,5 +39,31 @@ export class Viewer extends Entity {
 
   public setBiasIdols(ids: string[]) {
     this.mutable.viasIdolIds = ids;
+  }
+
+  public migrateToChzzk(migration: ViewerChzzkMigrationRequest) {
+    if (!this.twitchId || !this.twitchId.startsWith('!!chzzk')) {
+      throw new Error('Already migrated to chzzk');
+    }
+
+    const migrationLog = `!!chzzk${JSON.stringify(migration)}`;
+
+    this.mutable.twitchId = migrationLog;
+    this.mutable.username = migration.chzzkUsername;
+
+    migration.markAsMigrated();
+  }
+
+  public requestMigrationToChzzk(chzzkId: string, chzzkUsername: string) {
+    if (this.twitchId && !this.twitchId.startsWith('!!chzzk')) {
+      return new BusinessError('already-migrated');
+    }
+
+    return new ViewerChzzkMigrationRequest({
+      twitchId: this.twitchId || '',
+      twitchUsername: this.username,
+      chzzkId,
+      chzzkUsername,
+    });
   }
 }
